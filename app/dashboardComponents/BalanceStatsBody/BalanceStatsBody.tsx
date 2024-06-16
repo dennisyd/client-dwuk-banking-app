d9cc9475815a903126a19087831133b41543006c
dennisyd <dennisyd@alum.mit.edu>
2024-06-16 19:41:42 +0100

    feat: total money in the bank

--- app/dashboardComponents/BalanceStatsBody/BalanceStatsBody.tsx
+++ app/dashboardComponents/BalanceStatsBody/BalanceStatsBody.tsx
@@ -15,9 +15,23 @@ export default function BalanceStatsBody({ accounts }: BalanceStatsBodyProps) {
       else return balance;
     }, 0)
     .toString();
+
+  const loweBalance = accounts
+    .reduce((balance, account) => {
+      if (account.balance < balance) return account.balance;
+      else return balance;
+    }, Infinity)
+    .toString();
+
+  const totalMoney = accounts
+    .reduce((balance, account) => balance + account.balance, 0)
+    .toLocaleString();
+
   return (
     <Wrapper>
-      <Stat title="Higher Account Balance" score={higherBalance} />
+      <Stat title="Higher Account Balance:" score={`£ ${higherBalance}`} />
+      <Stat title="Lower Account Balance:" score={`£${loweBalance}`} />
+      <Stat title="Total Money in the Bank:" score={`£${totalMoney}`} />
     </Wrapper>
   );
 }
