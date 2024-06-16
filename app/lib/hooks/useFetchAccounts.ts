43b004f684e5155b41279d9d8e366ecf47b8f40b
dennisyd <dennisyd@alum.mit.edu>
2024-06-16 17:12:04 +0100

    chore

--- app/lib/hooks/useFetchAccounts.ts
+++ app/lib/hooks/useFetchAccounts.ts
@@ -3,25 +3,11 @@ import AccountProps from "../definitions/AuthorProps";
 import toast from "react-hot-toast";
 
 async function fetchAccounts(url: string): Promise<AccountProps[]> {
-  console.log("check 1");
-  // const res = await fetch(
-  //   "https://api-dwuk-banking-app-2c5a96dde0e1.herokuapp.com/accounts",
-  //   {
-  //     method: "GET",
-  //     headers: { "Content-Type": "application/json" },
-  //     credentials: "include"
-  //   }
-  // );
   const res = await fetch(url);
-  const headers = res.headers;
-  console.log("check 2 => headers", headers);
   if (!res.ok) {
-    console.log("check 3");
     toast.error("An error occurred while fetching the authors.");
   }
-  console.log("check 4");
   const accounts: AccountProps[] = await res.json();
-  console.log("accounts in fetch", accounts);
   return accounts;
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,10 @@
 import useFetchAccounts from "./lib/hooks/useFetchAccounts";
 
 export default function Dashboard() {
-  const url = process.env.API_URL + "/accounts";
-  // const url =
-  //   "https://api-dwuk-banking-app-2c5a96dde0e1.herokuapp.com/accounts";
-  // const url = "http://locahost:4000/accounts";
+  const apiBaseUrl = "https://api-dwuk-banking-app-2c5a96dde0e1.herokuapp.com";
+  const fetchAllAccountsUrl = apiBaseUrl + "/accounts";
 
-  const { accounts, setAccounts } = useFetchAccounts(url);
+  const { accounts, setAccounts } = useFetchAccounts(fetchAllAccountsUrl);
   return (
     <>
       <h1>Dashboard</h1>
