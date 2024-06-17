e47bc2a0faae6cd969bece98d793421ecdeef825
dennisyd <dennisyd@alum.mit.edu>
2024-06-17 06:41:13 +0100

    feat: new customer complete

--- app/customers/newCustomer/page.tsx
+++ app/customers/newCustomer/page.tsx
@@ -8,6 +8,32 @@ import {
   FieldProps
 } from "formik";
 import toast from "react-hot-toast";
+import styled from "styled-components";
+import inputStyle from "@/app/lib/common/formComponents/inputStyle";
+import Button from "@/app/lib/common/Button";
+import colours from "@/app/lib/constants/colors";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+`;
+const FieldsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 50vw;
+  margin: 1rem 0;
+`;
+
+const Input = styled(Field)`
+  ${inputStyle}
+`;
+
+const Header = styled.h1`
+  font-size: 6vw;
+`;
 
 interface NewCustomerFormProps {
   first_name: string;
@@ -24,25 +50,20 @@ export default function NewCustomer() {
 
   return (
     <div>
-      <h1>New Customer</h1>
       <Formik
         initialValues={initialValues}
         onSubmit={async (values, actions) => {
           const apiBaseUrl =
             "https://api-dwuk-banking-app-2c5a96dde0e1.herokuapp.com";
-          // "https://api-dwuk-banking-app-2c5a96dde0e1.herokuapp.com/customers"
           try {
-            const req = await fetch(
-              "https://api-dwuk-banking-app-2c5a96dde0e1.herokuapp.com/customers",
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                  Accept: "application/json"
-                },
-                body: JSON.stringify(values)
-              }
-            );
+            const req = await fetch(apiBaseUrl + "/customers", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+              },
+              body: JSON.stringify(values)
+            });
 
             if (req.ok) {
               toast.success(`${values.first_name} added successfully.`);
@@ -55,20 +76,27 @@ export default function NewCustomer() {
               toast.error("Something went wrong.");
             }
           }
-          console.log("values", values);
         }}
       >
         <Form>
-          <label htmlFor="first_name">First Name</label>
-          <Field id="first_name" name="first_name" placeholder="First Name" />
-
-          <label htmlFor="last_name">Last Name</label>
-          <Field id="last_name" name="last_name" placeholder="Last Name" />
-
-          <label htmlFor="email">Email</label>
-          <Field id="email" name="email" placeholder="Email" />
-
-          <button type="submit">Submit</button>
+          <Wrapper>
+            <Header>New Customer</Header>
+            <FieldsWrapper>
+              <Input
+                id="first_name"
+                name="first_name"
+                placeholder="First Name"
+              />
+              <Input id="last_name" name="last_name" placeholder="Last Name" />
+              <Input id="email" name="email" placeholder="Email" />
+            </FieldsWrapper>
+            <Button
+              type="submit"
+              text="Submit"
+              onClick={() => {}}
+              primaryColor={colours.black}
+            />
+          </Wrapper>
         </Form>
       </Formik>
     </div>
