7fab588eaef1f584b5dd35e9a263560bb616e0c0
dennisyd <dennisyd@alum.mit.edu>
2024-08-17 16:29:51 +0100

    feat: view accounts data flow

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { useAccountsWithCustomers } from "../lib/services/queries/queries";
+import Spinner from "../lib/components/common/Spinner/Spinner";
+import toast from "react-hot-toast";
+
 export default function Accounts() {
+  const accountsWithCustomers = useAccountsWithCustomers();
+
+  if (accountsWithCustomers.isPending) {
+    return <Spinner />;
+  }
+
+  if (accountsWithCustomers.isError) {
+    return toast.error("An error occurred while fetching accounts");
+  }
+
   return <h1>Accounts Page</h1>;
 }
