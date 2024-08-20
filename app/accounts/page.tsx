f7bd0f1ed9dc63e51e9f6c725f2800275a1955c8
dennisyd <dennisyd@alum.mit.edu>
2024-08-20 15:21:44 +0100

    feat/account actions intial setup

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -16,19 +16,27 @@ export default function Accounts() {
     return toast.error("An error occurred while fetching accounts");
   }
   return (
-    <div className={accountStyles.accountCardsContainer}>
-      {accountsWithCustomers.data.map((accountWithCustomer) => (
-        <AccountCard
-          key={accountWithCustomer.account_id}
-          account_id={accountWithCustomer.account_id}
-          first_name={accountWithCustomer.first_name}
-          last_name={accountWithCustomer.last_name}
-          balance={accountWithCustomer.balance}
-          open_date={accountWithCustomer.open_date}
-          last_activity_date={accountWithCustomer.last_activity_date}
-          status={accountWithCustomer.status}
-        />
-      ))}
+    <div>
+      <div>
+        <h3>Accounts Actions</h3>
+        <button>Top-Up Balance</button>
+        <button>Update Status</button>
+      </div>
+
+      <div className={accountStyles.accountCardsContainer}>
+        {accountsWithCustomers.data.map((accountWithCustomer) => (
+          <AccountCard
+            key={accountWithCustomer.account_id}
+            account_id={accountWithCustomer.account_id}
+            first_name={accountWithCustomer.first_name}
+            last_name={accountWithCustomer.last_name}
+            balance={accountWithCustomer.balance}
+            open_date={accountWithCustomer.open_date}
+            last_activity_date={accountWithCustomer.last_activity_date}
+            status={accountWithCustomer.status}
+          />
+        ))}
+      </div>
     </div>
   );
 }
