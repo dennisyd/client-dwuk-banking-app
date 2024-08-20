baad8a17a131fb0a8f98a8597dd9bb9ac9604f39
dennisyd <dennisyd@alum.mit.edu>
2024-08-20 08:24:37 +0100

    style: added pink colour

--- app/lib/constants/colors.ts
+++ app/lib/constants/colors.ts
@@ -1,8 +1,9 @@
 const colours = {
-  white: `rgb(255, 255, 255)`,
-  black: `rgb(0,0,0)`,
-  darkRed: `rgb(153, 0, 0)`,
-  gray: "rgb(128, 128, 128)"
+  white: "rgb(255, 255, 255)",
+  black: "rgb(0,0,0)",
+  darkRed: "rgb(153, 0, 0)",
+  gray: "rgb(128, 128, 128)",
+  pink: "rgb(254, 247, 247)"
 };
 
 export default colours;
