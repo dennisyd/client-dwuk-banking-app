c60259234711ddad7b7021a9bf45836080c908a1
dennisyd <dennisyd@alum.mit.edu>
2024-08-22 12:19:47 +0100

    style: added colours to the colorus file

--- app/lib/constants/colors.ts
+++ app/lib/constants/colors.ts
@@ -3,7 +3,9 @@ const colours = {
   black: "rgb(0,0,0)",
   darkRed: "rgb(153, 0, 0)",
   gray: "rgb(128, 128, 128)",
-  pink: "rgb(254, 247, 247)"
+  activeBubbleColor: "hsl(120, 61%, 30%)",
+  closedBubbleColor: "hsl(12, 100%, 30%)",
+  frozenBubbleColor: " hsl(48, 100%, 30%)"
 };
 
 export default colours;
