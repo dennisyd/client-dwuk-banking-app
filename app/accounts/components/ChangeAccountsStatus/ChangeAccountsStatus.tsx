9eb538dcd27bf71f1759db9b363e18f139dc950e
dennisyd <dennisyd@alum.mit.edu>
2024-08-22 12:06:46 +0100

    style: account actions outer container

--- app/accounts/components/ChangeAccountsStatus/ChangeAccountsStatus.tsx
+++ app/accounts/components/ChangeAccountsStatus/ChangeAccountsStatus.tsx
@@ -1,12 +1,16 @@
 import accountStatus from "@/app/lib/constants/accountStatus";
+import stylesChangeAccountStatus from "../../styles/changeAccountStatus.module.css";
 
 export default function ChangeAccountsStatus() {
   return (
-    <div>
-      Change Accounts Status to:
-      {accountStatus.map((status, i) => (
-        <button key={i}>{status}</button>
-      ))}
+    <div className={stylesChangeAccountStatus.accountActionsOuterContainer}>
+      <div>Change Accounts Status to:</div>
+
+      <div className={stylesChangeAccountStatus.actionButtonsContainer}>
+        {accountStatus.map((status, i) => (
+          <button key={i}>{status}</button>
+        ))}
+      </div>
     </div>
   );
 }
