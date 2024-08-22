7c9f5341920c6827f070aae99a816cd96662b9cd
dennisyd <dennisyd@alum.mit.edu>
2024-08-22 14:36:26 +0100

    test: update accounts status when user clicks on action buttons

--- app/accounts/components/UpdateAccountsStatus/__tests__/UpdateAccountsStatus.test.tsx
+++ app/accounts/components/UpdateAccountsStatus/__tests__/UpdateAccountsStatus.test.tsx
@@ -1,11 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import UpdateAccountsStatus from "../UpdateAccountsStatus";
+import userEvent from "@testing-library/user-event";
 
-test("if it renders account statuses as buttons", () => {
+test("if it renders account statuses as buttons", async () => {
   const updateAccountStatus = jest.fn();
   render(<UpdateAccountsStatus onUpdateAccountStatus={updateAccountStatus} />);
 
   const activeButton = screen.getByRole("button", { name: "Activate" });
   const closeButton = screen.getByRole("button", { name: "Close" });
   const freezeButton = screen.getByRole("button", { name: "Freeze" });
+
+  const user = userEvent.setup();
+
+  await user.click(activeButton);
+  expect(updateAccountStatus.mock.calls[0][0]).toBe("ACTIVE");
+
+  await user.click(closeButton);
+  expect(updateAccountStatus.mock.calls[1][0]).toBe("CLOSED");
+
+  await user.click(freezeButton);
+  expect(updateAccountStatus.mock.calls[2][0]).toBe("FROZEN");
 });
