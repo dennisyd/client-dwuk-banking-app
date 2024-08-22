eee3df9308fae42e4f0299380e39f5b4cc2a0c9e
dennisyd <dennisyd@alum.mit.edu>
2024-08-22 14:28:11 +0100

    test: UpdateAccountStatus component onUpdateAccountStatus

--- app/accounts/components/UpdateAccountsStatus/__tests__/UpdateAccountsStatus.test.tsx
+++ app/accounts/components/UpdateAccountsStatus/__tests__/UpdateAccountsStatus.test.tsx
@@ -2,7 +2,8 @@ import { render, screen } from "@testing-library/react";
 import UpdateAccountsStatus from "../UpdateAccountsStatus";
 
 test("if it renders account statuses as buttons", () => {
-  render(<UpdateAccountsStatus />);
+  const updateAccountStatus = jest.fn();
+  render(<UpdateAccountsStatus onUpdateAccountStatus={updateAccountStatus} />);
 
   const activeButton = screen.getByRole("button", { name: "Activate" });
   const closeButton = screen.getByRole("button", { name: "Close" });
